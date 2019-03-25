e093953b67921ef1786689ca331075c9aec557e3
Stefan Siegl <stesie@brokenpipe.de>
2019-03-25 21:55:36 +0100

    refactor: convert to FunctionComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import 'leaflet/dist/leaflet.css';
 import './App.css';
 
-import React, { Component } from 'react';
+import React, { FunctionComponent } from 'react';
 import { Query } from 'react-apollo';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 
@@ -9,40 +9,36 @@ import Session from './model/Session';
 import Popup from './Popup';
 import { FETCH_SESSIONS } from './queries';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App" style={{ height: '100vh', width: '100vw' }}>
-                <Map center={{ lng: 9.9602, lat: 49.7879 }} zoom={14} style={{ height: '100vh', width: '100vw' }}>
-                    <TileLayer
-                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Query<{ sessions: Session[] }> query={FETCH_SESSIONS}>
-                        {result => {
-                            if (!result.data || !result.data.sessions) {
-                                return null;
-                            }
+const App: FunctionComponent = () => (
+    <div className="App" style={{ height: '100vh', width: '100vw' }}>
+        <Map center={{ lng: 9.9602, lat: 49.7879 }} zoom={14} style={{ height: '100vh', width: '100vw' }}>
+            <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Query<{ sessions: Session[] }> query={FETCH_SESSIONS}>
+                {result => {
+                    if (!result.data || !result.data.sessions) {
+                        return null;
+                    }
 
-                            return result.data.sessions
-                                .filter(session => !session.cancelled)
-                                .filter(session => session.location && session.location.lat && session.location.lng)
-                                .map(session => {
-                                    return (
-                                        <Marker
-                                            key={session.key}
-                                            position={{ lng: session.location!.lng!, lat: session.location!.lat! }}
-                                        >
-                                            <Popup {...session} />
-                                        </Marker>
-                                    );
-                                });
-                        }}
-                    </Query>
-                </Map>
-            </div>
-        );
-    }
-}
+                    return result.data.sessions
+                        .filter(session => !session.cancelled)
+                        .filter(session => session.location && session.location.lat && session.location.lng)
+                        .map(session => {
+                            return (
+                                <Marker
+                                    key={session.key}
+                                    position={{ lng: session.location!.lng!, lat: session.location!.lat! }}
+                                >
+                                    <Popup {...session} />
+                                </Marker>
+                            );
+                        });
+                }}
+            </Query>
+        </Map>
+    </div>
+);
 
 export default App;
