9b964aaa7693cc518cd191d94e645416aad12cdb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-31 14:34:37 +0000

    Deploy stesie/wueww-map to github.com/stesie/wueww-map.git:gh-pages

---
+++ precache-manifest.8f0abb8f45e4767cc9f48a3002887cca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7f0bf91a50b90337ac7",
+    "url": "/wueww-map/static/css/main.12e79cfe.chunk.css"
+  },
+  {
+    "revision": "f7f0bf91a50b90337ac7",
+    "url": "/wueww-map/static/js/main.fcd2090a.chunk.js"
+  },
+  {
+    "revision": "1ac964e4bf8b3de2579d",
+    "url": "/wueww-map/static/js/runtime~main.7cbcc5d1.js"
+  },
+  {
+    "revision": "3658aecbb9865390c9cd",
+    "url": "/wueww-map/static/css/2.2c392383.chunk.css"
+  },
+  {
+    "revision": "3658aecbb9865390c9cd",
+    "url": "/wueww-map/static/js/2.e1b3c986.chunk.js"
+  },
+  {
+    "revision": "2726443f5704e9fa7aa4db288b84135c",
+    "url": "/wueww-map/index.html"
+  }
+];
